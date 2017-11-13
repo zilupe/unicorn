5f372f40677c81d847ab75e978df0a7ca27af1d7
Jazeps Basko <jazeps.basko@gmail.com>
2017-11-13 00:30:15 +0000

    More queries

--- unicorn/queries.sql
+++ unicorn/queries.sql
@@ -13,3 +13,17 @@ join (
 ) as aa on t.id = aa.away_team_id
 group by t.id;
 
+
+-- Number of points scored
+
+select t.id, t.name, hh.num_points + aa.num_points as total_points from
+teams as t
+join (
+  select g.home_team_id, sum(g.home_team_points) as num_points from games as g
+  group by g.home_team_id
+) as hh on t.id = hh.home_team_id
+join (
+  select g.away_team_id, sum(g.away_team_points) as num_points from games as g
+  group by g.away_team_id
+) as aa on t.id = aa.away_team_id
+group by t.id;
