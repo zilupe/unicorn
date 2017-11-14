27d5b44289af5b73e86e0a62ecb32c0833c47f24
Jazeps Basko <jazeps.basko@gmail.com>
2017-11-14 00:32:57 +0000

    Started on mappings and templates

---
+++ unicorn/generators/README.md
@@ -0,0 +1,4 @@
+* All games by each team
+* All games in a season
+* Table for each season
+* All-time table
---
+++ unicorn/mappings.py
@@ -0,0 +1,67 @@
+class Seasons:
+    Autumn2014 = 1
+    Winter2015 = 2
+    Spring2015 = 3
+    Summer2015 = 4
+    Autumn2015 = 5
+    LateAutumn2015 = 6
+    Winter2016 = 7
+    Spring2016 = 8
+    Summer2016 = 9
+    Autumn2016 = 10
+    LateAutumn2016 = 11
+    Winter2017 = 12
+    Spring2017 = 13
+    Summer2017 = 14
+    Autumn2017 = 15
+
+
+go_mammoth_seasons = {
+    Seasons.Autumn2014: 55,
+    Seasons.Winter2015: 59,
+    Seasons.Spring2015: 62,
+    Seasons.Summer2015: 70,
+    Seasons.Autumn2015: 73,
+    Seasons.LateAutumn2015: 79,
+    Seasons.Winter2016: 85,
+    Seasons.Spring2016: 80,
+    Seasons.Summer2016: 86,
+    Seasons.Autumn2016: 96,
+    Seasons.LateAutumn2016: 98,
+    Seasons.Winter2017: 100,
+    Seasons.Spring2017: 101,
+    Seasons.Summer2017: 102,
+    Seasons.Autumn2017: 104,
+}
+
+
+class Teams:
+    Supernova = 1
+    CamdenHells = 2
+    LostAngels = 3
+    Rockets = 4
+    Reloaded = 5
+    Burritos = 6
+    Ocelts = 7
+    AlleyOops = 8
+    Markit = 9
+    HaloHoops = 10
+    UpperStreetBallers = 11
+    LondonBulls = 12
+    IslingtonDevils = 13
+    N1Jam = 14
+
+
+go_mammoth_teams = {
+    Teams.CamdenHells: 3770,  # also called SWISH and MC2=SWISH etc.
+    Teams.LostAngels: 4896,  # from Summer2015
+    Teams.Supernova: 2716,  # first three seasons
+    # Teams.Rockets: ??
+}
+
+
+
+
+season_team_mappings = (
+
+)
