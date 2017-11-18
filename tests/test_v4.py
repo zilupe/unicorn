b1dffca57765ce505fefdc4aee5edc44fb8879d0
Jazeps Basko <jazeps.basko@gmail.com>
2017-11-18 22:58:52 +0000

    Basic GameSide model which allows easier access to one team's all games

---
+++ tests/test_v4.py
@@ -0,0 +1,37 @@
+from unicorn.models import Season, Team, Game, GameSide
+
+
+def test_models(db):
+    season = Season.create(name='Winter 2000')
+    team1 = Team.create(
+        id='{:0>4}.1'.format(season.id),
+        season=season
+    )
+    team2 = Team.create(
+        id='{:0>4}.2'.format(season.id),
+        season=season
+    )
+    team3 = Team.create(
+        id='{:0>4}.3'.format(season.id),
+        season=season
+    )
+
+    game12 = Game.create(
+        season=season,
+        sides=[
+            GameSide(team=team1),
+            GameSide(team=team2),
+        ],
+    )
+
+    game23 = Game.create(
+        season=season,
+        sides=[
+            GameSide(team=team2),
+            GameSide(team=team3),
+        ],
+    )
+
+    assert len(team1.games) == 1
+    assert len(team2.games) == 2
+    assert len(team3.games) == 1
--- unicorn/models.py
+++ unicorn/models.py
@@ -49,15 +49,46 @@ class Team(Base):
     st_bonus_points = Column(Integer)
     st_points = Column(Integer)
 
-    @property
-    def all_games(self):
-        all = []
-        all.extend(self.home_games)
-        all.extend(self.away_games)
-        return sorted(all, key=lambda g: g.starts_at, reverse=True)
+    games = relationship('GameSide')
 
 
 Team.default_order_by = Team.name.asc(),
+
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True)
+
+    season_id = Column(Integer, ForeignKey('seasons.id'))
+    season = relationship('Season', back_populates='games')
+
+    season_stage = Column(String(20), server_default=SeasonStages.regular)
+    starts_at = Column(DateTime)
+    notes = Column(Text)
+
+    sides = relationship('GameSide', back_populates='game')
+
+
+class GameSide(Base):
+    __tablename__ = 'game_sides'
+
+    id = Column(Integer, primary_key=True)
+
+    team_id = Column(String(15), ForeignKey('teams.id'), nullable=False)
+    team = relationship('Team', foreign_keys=team_id)
+
+    score = Column(Integer)
+    points = Column(Integer)
+    outcome = Column(String(5))
+
+    game_id = Column(Integer, ForeignKey('games.id'))
+    game = relationship('Game', back_populates='sides')
+
+    @property
+    def opposition(self):
+        # Should inspect game.sides and return the one that is not itself
+        raise NotImplementedError()
 
 
 class Season(Base):
@@ -74,43 +105,5 @@ class Season(Base):
 Season.default_order_by = Season.first_week_date.asc(),
 
 
-class Game(Base):
-    __tablename__ = 'games'
-
-    id = Column(Integer, primary_key=True)
-    season_id = Column(Integer, ForeignKey('seasons.id'))
-    season = relationship('Season', back_populates='games')
-    season_stage = Column(String(20), server_default=SeasonStages.regular)
-    starts_at = Column(DateTime)
-    home_team_id = Column(String(15), ForeignKey('teams.id'))
-    home_team_score = Column(Integer)
-    home_team_outcome = Column(String(5))
-    home_team_points = Column(Integer)
-    away_team_id = Column(String(15), ForeignKey('teams.id'))
-    away_team_score = Column(Integer)
-    away_team_outcome = Column(String(5))
-    away_team_points = Column(Integer)
-    notes = Column(Text)
-
-    home_team = relationship('Team', foreign_keys=[home_team_id], back_populates='home_games')
-    away_team = relationship('Team', foreign_keys=[away_team_id], back_populates='away_games')
-
-    def __repr__(self):
-        return '<{} {} - {}>'.format(self.__class__.__name__, self.home_team, self.away_team)
-
-    @property
-    def name(self):
-        return '{} - {}'.format(self.home_team.name, self.away_team.name)
-
-    @property
-    def link(self):
-        return '<a href="game_{}.html">{}</a>'.format(self.id, self.name)
-
-
-Game.default_order_by = Game.starts_at.asc(),
-
-
 Season.games = relationship('Game', order_by=Game.starts_at, back_populates='season')
-Team.home_games = relationship('Game', foreign_keys=Game.home_team_id, order_by=Game.starts_at, back_populates='home_team')
-Team.away_games = relationship('Game', foreign_keys=Game.away_team_id, order_by=Game.starts_at, back_populates='away_team')
 
